8e1d8f56c2048c1e09f72a09bfc95276516b6c73
PMwaniki <PMwaniki@kemri-wellcome.org>
2018-03-07 12:15:38 +0300

    Modified gallary to group images by date captured

--- src/containers/Gallery/Gallery.js
+++ src/containers/Gallery/Gallery.js
@@ -3,26 +3,42 @@ import {GridList, GridTile} from 'material-ui/GridList';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 import IconButton from 'material-ui/IconButton';
 import {connect} from 'react-redux';
+import Subheader from 'material-ui/Subheader';
 
 
 class Gallery extends Component{
 
     render(){
+        let dates=this.props.images.map(im=>im.date);
+        dates=[... new Set(dates)];
+        dates.sort((a,b)=>a<b ? 1:-1);
+        let elements=[];
+        dates.forEach(date=>{
+            elements.push(<Subheader>{date}</Subheader>);
+            let images2=this.props.images.filter(im=>im.date===date);
+            images2.forEach(image=>{
+                let tile= (<GridTile
+                    key={image.file_path}
+                    actionIcon={<IconButton><StarBorder color={this.props.synced_images.filter(sImage=>sImage.file_path===image.file_path).length>0? "green":"white"} /></IconButton>}
+                    title={image.record_id}
+                    subtitle={<span>by <b>{image.ipno}</b></span>}
+                >
+                    <img src={URL.createObjectURL(image.blob)} />
+
+                </GridTile>);
+
+                elements.push(tile);
+            });
+
+        });
+
+        console.log("Available dates",dates);
         return(
             <div>
                 <p>Images<StarBorder color="white" /></p>
                 <GridList>
-                  {this.props.images.map(image=>{
-                    return <GridTile
-                        key={image.file_path}
-                        actionIcon={<IconButton><StarBorder color={this.props.synced_images.filter(sImage=>sImage.file_path===image.file_path).length>0? "green":"white"} /></IconButton>}
-                        title={image.record_id}
-                        subtitle={<span>by <b>{image.ipno}</b></span>}
-                    >
-                        <img src={URL.createObjectURL(image.blob)} />
+                    {elements}
 
-                    </GridTile>
-                  })}
                 </GridList>
 
             </div>
