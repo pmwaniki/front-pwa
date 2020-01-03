5155f295b4827e757f20a0eab4f0c91d9e2620e8
Paul Mwaniki <pmmwaniki06@gmail.com>
2020-01-03 11:51:19 +0300

    changed development backend url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 
 export default {
-    backendURL: process.env.NODE_ENV === "production" ? "https://hsuweb.kemri-wellcome.org/verify" : "http://localhost:8000"
+    backendURL: process.env.NODE_ENV === "production" ? "https://hsuweb.kemri-wellcome.org/verify" : "http://localhost:8001"
 }
